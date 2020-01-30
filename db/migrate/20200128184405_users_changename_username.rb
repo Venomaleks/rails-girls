ff4c5ec29a0da7fdd288a0cd717dd13c916cb0c2
venomaleks <aleks.edholm@gmail.com>
2020-01-30 17:19:18 +0100

    add devisesidor,name

---
+++ db/migrate/20200128184405_users_changename_username.rb
@@ -0,0 +1,5 @@
+class UsersChangenameUsername < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :users, :user_name, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_133359) do
+ActiveRecord::Schema.define(version: 2020_01_29_123816) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -63,7 +63,9 @@ ActiveRecord::Schema.define(version: 2020_01_24_133359) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
